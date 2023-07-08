fcee2f6d2a1db23716cd5a4a77cce62a0a83802a
aranida14 <dinara1602@gmail.com>
2023-07-09 00:17:12 +0300

    fix linting errors

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,49 +1,51 @@
-import stringify from "./stringify.js";
+import stringify from './stringify.js';
 
 const stylish = (diff) => {
-    const replacer = ' ';
-    const spacesPerLevel = 4;
-    const leftOffset = 2;
-    const iter = (currentDiff, depth) => {
-        const indent = replacer.repeat(spacesPerLevel * depth - leftOffset);
-        const bracketIndent = replacer.repeat(spacesPerLevel * (depth - 1));
-        const nestedIndent = replacer.repeat(spacesPerLevel * depth);
-        const lines = currentDiff
-            .flatMap((item) => {
-                const { name, type, status, oldValue, newValue, children } = item;
-                let line;
-                if (type === 'plain') {
-                    switch (status) {
-                        case 'unchanged':
-                            line = `${indent}  ${name}: ${stringify(oldValue, replacer, spacesPerLevel, nestedIndent)}`;
-                            break;
-                        case 'changed':
-                            line = [];
-                            line.push(`${indent}- ${name}: ${stringify(oldValue, replacer, spacesPerLevel, nestedIndent)}`);
-                            line.push(`${indent}+ ${name}: ${stringify(newValue, replacer, spacesPerLevel, nestedIndent)}`);
-                            break;
-                        case 'added':
-                            line = `${indent}+ ${name}: ${stringify(newValue, replacer, spacesPerLevel, nestedIndent)}`;
-                            break;
-                        case 'deleted':
-                            line = `${indent}- ${name}: ${stringify(oldValue, replacer, spacesPerLevel, nestedIndent)}`;
-                            break;
-                        default:
-                            throw `Unknown status ${status}`;
-                    }   
-                } else {
-                    line = `${indent}  ${name}: ${iter(children, depth + 1)}`;
-                }
-                return line;
-            });
-        return [
-            '{',
-            ...lines,
-            `${bracketIndent}}`,
-        ].join('\n')
-    }
+  const replacer = ' ';
+  const spacesPerLevel = 4;
+  const leftOffset = 2;
+  const iter = (currentDiff, depth) => {
+    const indent = replacer.repeat(spacesPerLevel * depth - leftOffset);
+    const bracketIndent = replacer.repeat(spacesPerLevel * (depth - 1));
+    const nestedIndent = replacer.repeat(spacesPerLevel * depth);
+    const lines = currentDiff
+      .flatMap((item) => {
+        const {
+          name, type, status, oldValue, newValue, children,
+        } = item;
+        let line;
+        if (type === 'plain') {
+          switch (status) {
+            case 'unchanged':
+              line = `${indent}  ${name}: ${stringify(oldValue, replacer, spacesPerLevel, nestedIndent)}`;
+              break;
+            case 'changed':
+              line = [];
+              line.push(`${indent}- ${name}: ${stringify(oldValue, replacer, spacesPerLevel, nestedIndent)}`);
+              line.push(`${indent}+ ${name}: ${stringify(newValue, replacer, spacesPerLevel, nestedIndent)}`);
+              break;
+            case 'added':
+              line = `${indent}+ ${name}: ${stringify(newValue, replacer, spacesPerLevel, nestedIndent)}`;
+              break;
+            case 'deleted':
+              line = `${indent}- ${name}: ${stringify(oldValue, replacer, spacesPerLevel, nestedIndent)}`;
+              break;
+            default:
+              throw new Error(`Unknown status ${status}`);
+          }
+        } else {
+          line = `${indent}  ${name}: ${iter(children, depth + 1)}`;
+        }
+        return line;
+      });
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
 
-    return iter(diff, 1);
+  return iter(diff, 1);
 };
 
 export default stylish;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import stylish from './formatters/stylish.js';
 
 const isObject = (value) => typeof value === 'object' && value !== null;
 const formattersMap = {
-  'stylish': stylish,
-}
+  stylish,
+};
 
 export default (filepath1, filepath2, format = 'stylish') => {
   const fileObject1 = getFileObject(filepath1);
@@ -19,15 +19,23 @@ export default (filepath1, filepath2, format = 'stylish') => {
           name: key,
         };
         if (!Object.hasOwn(obj1, key)) {
-          singleDiff = { ...singleDiff, type: 'plain', status: 'added', newValue: obj2[key] };
+          singleDiff = {
+            ...singleDiff, type: 'plain', status: 'added', newValue: obj2[key],
+          };
         } else if (!Object.hasOwn(obj2, key)) {
-          singleDiff = { ...singleDiff, type: 'plain', status: 'deleted', oldValue: obj1[key] };
+          singleDiff = {
+            ...singleDiff, type: 'plain', status: 'deleted', oldValue: obj1[key],
+          };
         } else if (isObject(obj1[key]) && isObject(obj2[key])) {
           singleDiff = { ...singleDiff, type: 'complex', children: iter(obj1[key], obj2[key]) };
         } else if (obj1[key] === obj2[key]) {
-          singleDiff = { ...singleDiff, type: 'plain', status: 'unchanged', oldValue: obj1[key], newValue: obj2[key] };
+          singleDiff = {
+            ...singleDiff, type: 'plain', status: 'unchanged', oldValue: obj1[key], newValue: obj2[key],
+          };
         } else {
-          singleDiff = { ...singleDiff, type: 'plain', status: 'changed', oldValue: obj1[key], newValue: obj2[key] };
+          singleDiff = {
+            ...singleDiff, type: 'plain', status: 'changed', oldValue: obj1[key], newValue: obj2[key],
+          };
         }
         return singleDiff;
       });
