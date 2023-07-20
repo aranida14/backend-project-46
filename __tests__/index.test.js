b7a2f8e81aaeb46874fdfd8a55503abf23fe1d79
aranida14 <dinara1602@gmail.com>
2023-07-20 18:49:36 +0300

    fix tests and refactor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,32 +10,29 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let expectedResultStylish;
-let expectedResultPlain;
-let expectedResultJSON;
+const expectedResultStylish = readFile('expectedResultStylish.txt');
+const expectedResultPlain = readFile('expectedResultPlain.txt');
+const expectedResultJSON = readFile('expectedResultJSON.json');
 
-beforeAll(() => {
-  expectedResultStylish = readFile('expectedResultStylish.txt');
-  expectedResultPlain = readFile('expectedResultPlain.txt');
-  expectedResultJSON = readFile('expectedResultJSON.json');
-});
-
-test('json stylish diff', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedResultStylish);
-});
-
-test('yaml stylish diff', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expectedResultStylish);
-});
-
-test('json plain diff', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(expectedResultPlain);
-});
-
-test('yaml plain diff', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(expectedResultPlain);
-});
-
-test('json diff', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.json'), 'json')).toEqual(expectedResultJSON);
-});
+test
+  .each([
+    {
+      file1: 'file1.json', file2: 'file2.json', expected: expectedResultStylish, format: undefined, formatForTitle: 'stylish (default)',
+    },
+    {
+      file1: 'file1.yml', file2: 'file2.yml', expected: expectedResultStylish, format: undefined, formatForTitle: 'stylish (default)',
+    },
+    {
+      file1: 'file1.json', file2: 'file2.json', expected: expectedResultPlain, format: 'plain', formatForTitle: 'plain',
+    },
+    {
+      file1: 'file1.yml', file2: 'file2.yml', expected: expectedResultPlain, format: 'plain', formatForTitle: 'plain',
+    },
+    {
+      file1: 'file1.yml', file2: 'file2.json', expected: expectedResultJSON, format: 'json', formatForTitle: 'json',
+    },
+  ])('testing diff ($file1, $file2) with format $formatForTitle', ({
+    file1, file2, expected, format,
+  }) => {
+    expect(genDiff(getFixturePath(file1), getFixturePath(file2), format)).toEqual(expected);
+  });
